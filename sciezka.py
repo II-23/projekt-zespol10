384f6893b5c1d767a94ffd9796e4da30cd6550c8
mlodyhubson <114824055+mlodyhubson@users.noreply.github.com>
2024-01-08 15:02:30 +0100

    "sciezka" zamienilem na "SCIEZKA" bo tak jest w stale.py

--- sciezka.py
+++ sciezka.py
@@ -1,6 +1,7 @@
 import pygame
 from stale import *
 
+
 class Kwadrat(pygame.sprite.Sprite):
     def __init__(self, color, size, row, col):
         super().__init__()
@@ -9,23 +10,26 @@ class Kwadrat(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (col * size, row * size)
 
+
 sciezka = pygame.sprite.Group()
-waypoints = [[12,0]]
-kwadrat1 = Kwadrat((255, 0, 0), siatka, waypoints[0][1], waypoints[0][0])
+waypoints = [[12, 0]]
+kwadrat1 = Kwadrat((255, 0, 0), SIATKA, waypoints[0][1], waypoints[0][0])
 sciezka.add(kwadrat1)
-kwadrat1 = Kwadrat((255, 0, 0), siatka, 9, 0)
+kwadrat1 = Kwadrat((255, 0, 0), SIATKA, 9, 0)
 sciezka.add(kwadrat1)
+
 
 def dodajKwadrat():
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    col = mouse_x // siatka
-    row = mouse_y // siatka
+    col = mouse_x // SIATKA
+    row = mouse_y // SIATKA
 
-    if (waypoints[-1][0]==col or waypoints[-1][1]==row) and [col,row] not in waypoints and [0,9] not in waypoints:
+    if (waypoints[-1][0] == col or waypoints[-1][1] == row) and [col, row] not in waypoints and [0, 9] not in waypoints:
         waypoints.append([col, row])
         print(waypoints)
-        kwadrat = Kwadrat((255, 0, 0), siatka, row, col)
+        kwadrat = Kwadrat((255, 0, 0), SIATKA, row, col)
         sciezka.add(kwadrat)
+
 
 def wypelnienieSciezki():
     for i in range(len(waypoints) - 1):
@@ -34,16 +38,17 @@ def wypelnienieSciezki():
 
         if start_row == end_row:
             for col in range(min(start_col, end_col), max(start_col, end_col) + 1):
-                kwadrat = Kwadrat((255, 0, 0), siatka, start_row, col)
+                kwadrat = Kwadrat((255, 0, 0), SIATKA, start_row, col)
                 sciezka.add(kwadrat)
 
         elif start_col == end_col:
             for row in range(min(start_row, end_row), max(start_row, end_row) + 1):
-                kwadrat = Kwadrat((255, 0, 0), siatka, row, start_col)
-                sciezka.add(kwadrat)    
-        
-    if [0,9] in waypoints:
+                kwadrat = Kwadrat((255, 0, 0), SIATKA, row, start_col)
+                sciezka.add(kwadrat)
+
+    if [0, 9] in waypoints:
         print("Pelna sciezka")
+
 
 def rysujSciezke(screen):
     sciezka.draw(screen)
