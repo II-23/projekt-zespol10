8371758b42f3d7cdf33fddd57564283c6a010377
majaomelaniuk <150002963+majaomelaniuk@users.noreply.github.com>
2024-01-10 19:49:38 +0100

    Update klasy wrogowie

--- wrogowie.py
+++ wrogowie.py
@@ -1,13 +1,41 @@
 import pygame as pg
+from pygame.math import Vector2
+import math
 
 class Wrog(pg.sprite.Sprite):
-  def __init__(self, pos, image):
+  def __init__(self, waypoints, image):
     pg.sprite.Sprite.__init__(self)
-    self.image = image
+    self.waypoints = waypoints
+    self.pos = Vector2(self.waypoints[0])
+    self.target_waypoint = 1
+    self.speed = 2
+    self.angle = 0
+    self.original_image = image
+    self.image = pg.transform.rotate(self.original_image, self.angle)
     self.rect = self.image.get_rect()
-    self.rect.center = pos
+    self.rect.center = self.pos
 
   def update(self):
     self.move()
+    self.rotate()
+
   def move(self):
-    self.rect.x += 1
+    if self.target_waypoint < len(self.waypoints):
+      self.target = Vector2(self.waypoints[self.target_waypoint])
+      self.movement = self.target - self.pos
+    else:
+      self.kill()
+    dist = self.movement.length()
+    if dist >= self.speed:
+      self.pos += self.movement.normalize() * self.speed
+    else:
+      if dist != 0:
+        self.pos += self.movement.normalize() * dist
+      self.target_waypoint += 1
+
+  def rotate(self):
+    dist = self.target - self.pos
+    self.angle = math.degrees(math.atan2(-dist[1], dist[0]))
+    self.image = pg.transform.rotate(self.original_image, self.angle)
+    self.rect = self.image.get_rect()
+    self.rect.center = self.pos
