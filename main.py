6c14b084e236e0eaa436accfe72699bc751988ad
Piotr Pijanowski <piotr.pijanowski5@gmail.com>
2024-01-21 22:38:09 +0100

    Kolejny raz naprawiam błąd związany z sojusznikami

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ while running:
                             wybrana_wieza = Wieza.wybierz_wieze(mouse_pos, wieze)   
             else:
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-                    if pygame.mouse.get_pos()[0]<800 and game.kasa >= 200:
+                    if pygame.mouse.get_pos()[0]<800 and game.kasa >= sojusznik.price:
                         sojusznik = Sojusznik(pygame.mouse.get_pos(), sojusznik_image)
                         sojusznicy.add(sojusznik)
                         game.kasa -= sojusznik.price
